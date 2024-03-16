ca8289ca77b02e379a40c48daddc6255a53a934b
mozidan27 <153581941+mozidan27@users.noreply.github.com>
2024-03-16 13:16:10 +0200

    expanded add to the column

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -16,46 +16,48 @@ class HomePage extends StatelessWidget {
         ),
         backgroundColor: const Color(0xff070707),
       ),
-      body: const Center(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 40),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 170,
-              ),
-              Text(
-                "See what's happening in the world right now.",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 31,
-                  fontFamily: 'Chirp',
+      body: const Expanded(
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 170,
                 ),
-              ),
-              SizedBox(
-                height: 150,
-              ),
-              CustomButtom(
-                buttomText: 'Continue with Google',
-                image: 'assets/images/Google__G__logo.svg.png',
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              CustomButtom(
-                buttomText: 'Continue with Apple',
-                image: 'assets/images/apple-logo-transparent.png',
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              CustomButtom(
-                buttomText: 'Create account',
-              ),
-            ],
+                Text(
+                  "See what's happening in the world right now.",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 31,
+                    fontFamily: 'Chirp',
+                  ),
+                ),
+                SizedBox(
+                  height: 150,
+                ),
+                CustomButtom(
+                  buttomText: 'Continue with Google',
+                  image: 'assets/images/Google__G__logo.svg.png',
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                CustomButtom(
+                  buttomText: 'Continue with Apple',
+                  image: 'assets/images/apple-logo-transparent.png',
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                CustomButtom(
+                  buttomText: 'Create account',
+                ),
+              ],
+            ),
           ),
         ),
       ),
