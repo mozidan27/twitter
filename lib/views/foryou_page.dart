294f6d37fa03908791f294ef47da5f6a348e3eac
mozidan27 <153581941+mozidan27@users.noreply.github.com>
2024-03-18 19:33:37 +0200

    replace column with list view

--- lib/views/foryou_page.dart
+++ lib/views/foryou_page.dart
@@ -9,124 +9,122 @@ class ForYou extends StatelessWidget {
       backgroundColor: Colors.black,
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: Expanded(
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Container(
-                    height: 60,
-                    width: 60,
-                    decoration: const BoxDecoration(
-                        shape: BoxShape.circle, color: Colors.white),
-                    child: Image.asset(
-                      'assets/images/download.jpg',
-                      fit: BoxFit.cover,
-                    ),
+        child: ListView(
+          children: [
+            Row(
+              children: [
+                Container(
+                  height: 60,
+                  width: 60,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.white),
+                  child: Image.asset(
+                    'assets/images/download.jpg',
+                    fit: BoxFit.cover,
                   ),
-                  const SizedBox(
-                    width: 10,
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                const Text(
+                  'Mohamed Zidan',
+                  style: TextStyle(color: Colors.white),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  '@mozidan27',
+                  style: TextStyle(color: Colors.grey[700]),
+                ),
+                const Spacer(),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.more_horiz,
+                    color: Colors.grey[700],
                   ),
-                  const Text(
-                    'Mohamed Zidan',
-                    style: TextStyle(color: Colors.white),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Hello this is zidan and i'm new flutter dev please give me some support",
+              style: TextStyle(color: Colors.white),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 60),
+              child: Image.asset('assets/images/download.jpg'),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(left: 25),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.mode_comment_outlined,
+                    color: Colors.grey[700],
                   ),
-                  const SizedBox(
-                    width: 10,
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.share,
+                    color: Colors.grey[700],
                   ),
-                  Text(
-                    '@mozidan27',
-                    style: TextStyle(color: Colors.grey[700]),
+                  onPressed: () {
+                    // Add your share button onPressed logic here
+                  },
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.favorite,
+                    color: Colors.grey[700],
                   ),
-                  const Spacer(),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.more_horiz,
-                      color: Colors.grey[700],
-                    ),
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text(
-                "Hello this is zidan and i'm new flutter dev please give me some support",
-                style: TextStyle(color: Colors.white),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                padding: const EdgeInsets.only(left: 60),
-                child: Image.asset('assets/images/download.jpg'),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(left: 25),
+                  onPressed: () {
+                    // Add your like button onPressed logic here
+                  },
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.bar_chart_outlined,
+                    color: Colors.grey[700],
                   ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.mode_comment_outlined,
-                      color: Colors.grey[700],
-                    ),
-                    onPressed: () {},
+                  onPressed: () {
+                    // Add your like button onPressed logic here
+                  },
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.bookmark_border_outlined,
+                    color: Colors.grey[700],
                   ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.share,
-                      color: Colors.grey[700],
-                    ),
-                    onPressed: () {
-                      // Add your share button onPressed logic here
-                    },
+                  onPressed: () {
+                    // Add your like button onPressed logic here
+                  },
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.ios_share_outlined,
+                    color: Colors.grey[700],
                   ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.favorite,
-                      color: Colors.grey[700],
-                    ),
-                    onPressed: () {
-                      // Add your like button onPressed logic here
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.bar_chart_outlined,
-                      color: Colors.grey[700],
-                    ),
-                    onPressed: () {
-                      // Add your like button onPressed logic here
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.bookmark_border_outlined,
-                      color: Colors.grey[700],
-                    ),
-                    onPressed: () {
-                      // Add your like button onPressed logic here
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.ios_share_outlined,
-                      color: Colors.grey[700],
-                    ),
-                    onPressed: () {
-                      // Add your like button onPressed logic here
-                    },
-                  ),
-                ],
-              ),
-            ],
-          ),
+                  onPressed: () {
+                    // Add your like button onPressed logic here
+                  },
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
