51ad462056040a1c0cc9c45515b77317f0ef23da
mozidan27 <153581941+mozidan27@users.noreply.github.com>
2024-03-22 16:35:19 +0200

    search page completed

--- lib/views/pages/search_page.dart
+++ lib/views/pages/search_page.dart
@@ -1,35 +1,107 @@
 import 'package:flutter/material.dart';
-import 'package:twitter/widgets/custom_text_field.dart';
+import 'package:flutter/widgets.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 80),
-            child: Center(
-              child: SizedBox(
-                width: 300,
-                height: 60,
-                child: TextField(
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    hintText: '🔍 Search',
-                    hintStyle: TextStyle(color: Colors.grey[700]),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(100),
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: CustomScrollView(
+          scrollDirection: Axis.vertical,
+          slivers: [
+            SliverAppBar(
+              leading: const Icon(
+                Icons.account_circle,
+                color: Colors.white,
+              ),
+              actions: [
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.settings_outlined,
+                      color: Colors.white,
+                    ))
+              ],
+              backgroundColor: const Color(0xff070707),
+              expandedHeight: 50,
+              centerTitle: true,
+              title: SizedBox(
+                  height: 50,
+                  width: 300,
+                  child: TextField(
+                    style: const TextStyle(color: Colors.white),
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      hintText: '🔍 Search',
+                      hintStyle: TextStyle(color: Colors.grey[700]),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(100),
+                      ),
                     ),
-                  ),
+                  )),
+            ),
+            SliverToBoxAdapter(
+              child: Expanded(
+                child: Column(
+                  children: [
+                    TabBar(
+                      automaticIndicatorColorAdjustment: true,
+                      labelColor: Colors.white,
+                      unselectedLabelColor: Colors.grey[800],
+                      isScrollable: true,
+                      indicatorColor: const Color(0xff1d9bf0),
+                      indicatorSize: TabBarIndicatorSize.label,
+                      indicatorWeight: 5,
+                      dividerColor: const Color(0xFF212121),
+                      dividerHeight: 1.3,
+                      tabs: const [
+                        Tab(
+                          child: Text(
+                            'For you',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w600),
+                          ),
+                        ),
+                        Tab(
+                          child: Text(
+                            'Trending',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w600),
+                          ),
+                        ),
+                        Tab(
+                          child: Text(
+                            'News',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w600),
+                          ),
+                        ),
+                        Tab(
+                          child: Text(
+                            'Sports',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w600),
+                          ),
+                        ),
+                        Tab(
+                          child: Text(
+                            'Entertainment',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w600),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
